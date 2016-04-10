3e884ee850fff0a9d2e17d9bbf49a9a2e0461394
Lee <theboss@TheBosss-Mac-mini.local>
2016-04-09 20:53:40 -0400

    Initial commit
    Nothing done yet

---
+++ Graph.java
@@ -0,0 +1,13 @@
+package graph;
+
+/**
+ *
+ * @author theboss
+ */
+public class Graph {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    
+}
