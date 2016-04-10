443defcc8852dc45b2d420553ba94577204a3970
Lee <theboss@TheBosss-Mac-mini.local>
2016-04-09 22:35:54 -0400

    Add drawing axes and improve test case

--- Graph.java
+++ Graph.java
@@ -1,10 +1,15 @@
 package graph;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.Point;
 import java.util.ArrayList;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
@@ -21,8 +26,20 @@ public class Graph {
         LineGraph lg = new LineGraph(p, "X", "Y", Color.BLUE);
         
         JFrame f = new JFrame("A Graph");
-        
-        f.add(lg);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container contentPane = f.getContentPane();
+        JPanel jp = new JPanel();
+        jp.setLayout(new GridLayout(3,3));
+        contentPane.add(jp);
+        jp.add(new JLabel("a"));
+        jp.add(new JLabel("b"));
+        jp.add(new JLabel("c"));
+        jp.add(new JLabel("d"));
+        jp.add(lg);
+        jp.add(new JLabel("f"));
+        jp.add(new JLabel("g"));
+        jp.add(new JLabel("h"));
+        jp.add(new JLabel("i"));
         
         f.setPreferredSize(new Dimension(600, 700));
         f.setLocationRelativeTo(null);
--- LineGraph.java
+++ LineGraph.java
@@ -6,21 +6,24 @@
 package graph;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Point;
 import java.util.ArrayList;
-import javax.swing.JComponent;
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
 
 /**
  *
- * LineGraph class should extend Graph which should extend JComponent but for now we will skip that
+ * LineGraph class should extend Graph which should extend JComponent but for
+ * now we will skip that
  */
-public class LineGraph extends JComponent{
-    
+public class LineGraph extends JPanel {
+
     ArrayList<Point> points;
     String xAxis;
     String yAxis;
     Color colour;
-    
+
     /*
      * LineGraph constructor creates a LineGraph object to be drawn to the screen
      * 
@@ -29,21 +32,40 @@ public class LineGraph extends JComponent{
      * @param yAxis         String label for the y axis of the line graph
      * @param colour        Color object to use when drawing the line graph
      */
-    public LineGraph(ArrayList<Point> points, String xAxis, String yAxis, Color colour){
+    public LineGraph(ArrayList<Point> points, String xAxis, String yAxis, Color colour) {
         this.points = points;
         this.xAxis = xAxis;
         this.yAxis = yAxis;
         this.colour = colour;
+        this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
     }
-    
+
     /*
      * LineGraph empty constructor creates a blank LineGraph object with default values
      */
-    public LineGraph(){
+    public LineGraph() {
         this.points = new ArrayList();
         this.xAxis = "";
         this.yAxis = "";
         this.colour = Color.BLACK;
     }
     
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        int BORDER_OFFSET = 15; // number of pixels from border to draw axes of graph
+        
+        g.setColor(Color.BLACK);
+        
+        // draw x axis for line graph
+        g.drawLine(BORDER_OFFSET, this.getBounds().height - BORDER_OFFSET, this.getBounds().width - BORDER_OFFSET, this.getBounds().height - BORDER_OFFSET); // x1, y1, x2, y2
+        
+        // draw y axis for line graph
+        g.drawLine(BORDER_OFFSET, BORDER_OFFSET, BORDER_OFFSET, this.getBounds().height - BORDER_OFFSET); // x1, y1, x2, y2
+        
+        System.out.println("width: "+this.getBounds().width + ", height: "+this.getBounds().height);
+
+    }
+
 }
